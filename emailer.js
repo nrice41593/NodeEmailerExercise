c5a2165a5e13398dde7aefcf748400ef3e561b0a
Nathan Rice <nrice778@gmail.com>
2020-12-15 14:03:24 -0600

    exercise complete

---
+++ emailer.js
@@ -0,0 +1,18 @@
+// TODO npm install and require the nodemailer module
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  // Complete the authorization part of this code 
+});
+
+var mailOptions = {
+  // TODO: Complete the following: from, to, subject, text
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
